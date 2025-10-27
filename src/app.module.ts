beaf48c12447477672f00e97ad63b32f10e83c1b
fuadashraful <fuadashraful16@gmail.com>
2025-10-27 17:15:10 +0600

    feat: Mongodb module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthorsModule } from './authors/author.module';
+import databaseConfig from './database/config/database.config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from './database/mongoose-config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        databaseConfig,
+      ],
+      envFilePath: ['.env'],
+    }),
+    MongooseModule.forRootAsync({
+      useClass: MongooseConfigService,
+    }),
+    AuthorsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
